527cd7be39a4580b7a23fa758cf6ec3cf1fd7ce4
AldarRad <rald2005@mail.ru>
2024-03-05 15:53:41 +0700

    first_commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <random>
+int main()
+{
+	const int n=3;
+	int s=0;
+	int mas[n][n];
+	std::cout << "";
+	for (int i = 0;i<n;i++)
+	{
+		for (int j=0;j<n;j++)
+		{
+			std::cin>>mas[i][j];
+		}
+		
+	}
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n;j++)
+		{
+			std::cout << mas[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+	for (int i = 0;i < n;i++)
+	{
+		if (mas[i][i] == 0) {
+			for (int j = 0;j < n;j++)
+			{
+				s += mas[i][j];				
+			}
+			mas[i][i]= s;
+			s = 0;
+		}
+	}
+	for (int i=0;i<n;i++)
+	{
+		for (int j=0;j<n;j++)
+		{
+			std::cout << mas[i][j]<<" ";
+		}
+		std::cout << std::endl;
+	}
+}
